43cd03db98823858c2f3e50dd69f83179d2abbed
Lucien Müller <lcnmllr@gmail.com>
2026-01-12 23:51:19 +0100

    feat!: release 0.1.0

---
+++ src/lmtetris.rs
@@ -0,0 +1,532 @@
+use std::cmp::Eq;
+use std::ops::Add;
+
+use ratatui::style::palette::material::YELLOW;
+
+const TETS: [[Point; 4]; 7] = [I, J, L, O, S, T, Z];
+
+const I: [Point; 4] = [
+    Point { x: -1, y: 0 },
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 0 },
+    Point { x: 2, y: 0 },
+];
+const J: [Point; 4] = [
+    Point { x: -1, y: 0 },
+    Point { x: -1, y: 1 },
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 0 },
+];
+const L: [Point; 4] = [
+    Point { x: -1, y: 0 },
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 0 },
+    Point { x: 1, y: 1 },
+];
+const O: [Point; 4] = [
+    Point { x: 0, y: 0 },
+    Point { x: 0, y: 1 },
+    Point { x: 1, y: 0 },
+    Point { x: 1, y: 1 },
+];
+const S: [Point; 4] = [
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 0 },
+    Point { x: -1, y: 1 },
+    Point { x: 0, y: 1 },
+];
+const T: [Point; 4] = [
+    Point { x: -1, y: 0 },
+    Point { x: 0, y: 0 },
+    Point { x: 1, y: 0 },
+    Point { x: 0, y: 1 },
+];
+const Z: [Point; 4] = [
+    Point { x: -1, y: 0 },
+    Point { x: 0, y: 0 },
+    Point { x: 0, y: 1 },
+    Point { x: 1, y: 1 },
+];
+
+#[derive(Clone, Default, Debug)]
+pub struct Color {
+    pub c: u8,
+}
+
+#[derive(Clone, Debug)]
+struct Tile {
+    filled: bool,
+    color: Color,
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Point {
+    fn from(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
+    fn rotate_around_quarter(&mut self, orig: &Point, dir: &Direction) {
+        let mut p_relative = *self;
+        match dir {
+            Direction::Left => {
+                if p_relative.x > 0 {
+                    if p_relative.y > 0 {
+                        p_relative.x = -p_relative.x;
+                    } else {
+                        p_relative.y = -p_relative.y;
+                    }
+                } else {
+                    if p_relative.y > 0 {
+                        p_relative.y = -p_relative.y;
+                    } else {
+                        p_relative.x = -p_relative.x;
+                    }
+                }
+            }
+            Direction::Right => {
+                if p_relative.x > 0 {
+                    if p_relative.y > 0 {
+                        p_relative.y = -p_relative.y;
+                    } else {
+                        p_relative.x = -p_relative.x;
+                    }
+                } else {
+                    if p_relative.y > 0 {
+                        p_relative.x = -p_relative.x;
+                    } else {
+                        p_relative.y = -p_relative.x;
+                    }
+                }
+            }
+            _ => (),
+        }
+        *self = p_relative;
+    }
+
+    fn mov(&mut self, dir: &Direction) {
+        match dir {
+            Direction::Left => *self = *self + (-1, 0),
+            Direction::Right => *self = *self + (1, 0),
+            Direction::Up => *self = *self + (0, 1),
+            Direction::Down => *self = *self + (0, -1),
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Add<(i32, i32)> for Point {
+    type Output = Point;
+    fn add(self, other: (i32, i32)) -> Point {
+        Point {
+            x: self.x + other.0,
+            y: self.y + other.1,
+        }
+    }
+}
+
+impl std::ops::Sub for Point {
+    type Output = Point;
+
+    fn sub(self, other: Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+enum TetrominoVariant {
+    #[default]
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
+}
+
+impl TetrominoVariant {
+    fn to_index(self) -> usize {
+        match self {
+            TetrominoVariant::I => 0,
+            TetrominoVariant::J => 1,
+            TetrominoVariant::L => 2,
+            TetrominoVariant::O => 3,
+            TetrominoVariant::S => 4,
+            TetrominoVariant::T => 5,
+            TetrominoVariant::Z => 6,
+        }
+    }
+    fn by_index(i: usize) -> Self {
+        match i {
+            0 => TetrominoVariant::I,
+            1 => TetrominoVariant::J,
+            2 => TetrominoVariant::L,
+            3 => TetrominoVariant::O,
+            4 => TetrominoVariant::S,
+            5 => TetrominoVariant::T,
+            6 => TetrominoVariant::Z,
+            _ => panic!("No Variant i=\"{i}\"!"),
+        }
+    }
+    fn random() -> Self {
+        TetrominoVariant::by_index(rand::random_range(0..7))
+    }
+}
+
+#[derive(Default, Clone, PartialEq, Eq)]
+enum Rotation {
+    #[default]
+    Deg0,
+    Deg90,
+    Deg180,
+    Deg270,
+}
+
+impl Rotation {
+    fn rotate(&mut self, d: &Direction) {
+        match self {
+            Rotation::Deg0 => {
+                if *d == Direction::Right {
+                    *self = Rotation::Deg90;
+                }
+                if *d == Direction::Left {
+                    *self = Rotation::Deg270;
+                }
+            }
+            Rotation::Deg90 => {
+                if *d == Direction::Right {
+                    *self = Rotation::Deg180;
+                }
+                if *d == Direction::Left {
+                    *self = Rotation::Deg0;
+                }
+            }
+            Rotation::Deg180 => {
+                if *d == Direction::Right {
+                    *self = Rotation::Deg270;
+                }
+                if *d == Direction::Left {
+                    *self = Rotation::Deg90;
+                }
+            }
+            Rotation::Deg270 => {
+                if *d == Direction::Right {
+                    *self = Rotation::Deg0;
+                }
+                if *d == Direction::Left {
+                    *self = Rotation::Deg180;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct Tetromino {
+    variant: TetrominoVariant,
+    center: Point,
+    rotation: Rotation,
+    pub color: Color,
+}
+
+impl Tetromino {
+    fn random(x: i32, y: i32) -> Self {
+        let variant = TetrominoVariant::random();
+        Self {
+            variant,
+            center: Point { x, y },
+            rotation: Rotation::Deg0,
+            color: Color {
+                c: 9 + variant.to_index() as u8,
+            },
+        }
+    }
+    pub fn points(&self) -> [Point; 4] {
+        let mut tet = TETS[self.variant.to_index()];
+        for p in &mut tet {
+            let p_prev = p.clone();
+            match self.rotation {
+                Rotation::Deg0 => (),
+                Rotation::Deg90 => {
+                    p.x = p_prev.y;
+                    p.y = -p_prev.x;
+                }
+                Rotation::Deg180 => {
+                    p.x = -p_prev.x;
+                    p.y = -p_prev.y;
+                }
+                Rotation::Deg270 => {
+                    p.x = -p_prev.y;
+                    p.y = p_prev.x;
+                }
+            }
+            *p = *p + self.center;
+        }
+        tet
+    }
+    fn fall(&mut self) {
+        self.center = self.center + (0, 1);
+    }
+    fn collides(&self, board: &Board) -> bool {
+        for p in self.points() {
+            if p.y < 0 {
+                return true;
+            }
+            if p.y >= board.height {
+                return true;
+            }
+            if p.x < 0 {
+                return true;
+            }
+            if p.x >= board.width {
+                return true;
+            }
+            if board.get_tile(p).filled {
+                return true;
+            }
+        }
+        false
+    }
+    fn rotate(&mut self, dir: &Direction) -> bool {
+        self.rotation.rotate(dir);
+        true
+    }
+    fn mov(&mut self, dir: &Direction) -> bool {
+        self.center.mov(dir);
+        true
+    }
+}
+
+#[derive(Debug)]
+struct Board {
+    tiles: Vec<Tile>,
+    width: i32,
+    height: i32,
+}
+
+#[derive(PartialEq)]
+enum GameState {
+    Running,
+    Paused,
+    GameOver,
+}
+
+pub struct Tetris {
+    state: GameState,
+    board: Board,
+    tetromino: Tetromino,
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+impl Board {
+    fn new(width: i32, height: i32) -> Self {
+        assert!(width >= 4);
+        assert!(height >= 4);
+        Board {
+            tiles: vec![
+                Tile {
+                    filled: false,
+                    color: Color { c: 0 },
+                };
+                (width * height) as usize
+            ],
+            width,
+            height,
+        }
+    }
+    fn p2i(&self, p: &Point) -> usize {
+        assert!(p.x >= 0);
+        assert!(p.y >= 0);
+        assert!(p.x < self.width);
+        assert!(p.y < self.height);
+        (p.x + p.y * self.width) as usize
+    }
+    fn get_tile(&self, p: Point) -> &Tile {
+        &self.tiles[self.p2i(&p)]
+    }
+    fn fill_tetromino_check_gameover(&mut self, tet: &Tetromino) -> bool {
+        for p in &tet.points() {
+            let i = self.p2i(p);
+            let t = &mut self.tiles[i];
+            if t.filled {
+                return true;
+            } else {
+                t.filled = true;
+                t.color = tet.color.clone();
+            }
+        }
+        false
+    }
+    fn check_and_remove_lines(&mut self) {
+        let mut c = 0;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let p = Point::from(x, y);
+                if self.tiles[self.p2i(&p)].filled {
+                    c += 1;
+                }
+            }
+            if c == self.width {
+                self.remove_line_and_drop(y);
+            }
+            c = 0;
+        }
+    }
+    fn remove_line_and_drop(&mut self, y: i32) {
+        for y in (1..=y).rev() {
+            for x in 0..self.width {
+                let i = self.p2i(&Point::from(x, y));
+                let i_top = self.p2i(&Point::from(x, y - 1));
+                let tile_top = self.tiles[i_top].clone();
+                let tile = &mut self.tiles[i];
+                tile.filled = tile_top.filled;
+                tile.color = tile_top.color;
+            }
+        }
+    }
+}
+
+impl Tetris {
+    pub fn new(width: i32, height: i32) -> Self {
+        Tetris {
+            state: GameState::Paused,
+            board: Board::new(width, height),
+            tetromino: Tetromino::random(width / 2, 0),
+        }
+    }
+    pub fn dimensions(&self) -> (i32, i32) {
+        (self.board.width, self.board.height)
+    }
+    pub fn start(&mut self) {
+        self.state = GameState::Running
+    }
+    pub fn stop(&mut self) {
+        self.state = GameState::Paused
+    }
+    pub fn is_running(&self) -> bool {
+        self.state == GameState::Running
+    }
+    pub fn is_gameover(&self) -> bool {
+        self.state == GameState::GameOver
+    }
+    pub fn move_tet(&mut self, mov: Option<Direction>, rot: Option<Direction>) -> bool {
+        let mut tet_next = self.tetromino.clone();
+
+        if let Some(r) = rot {
+            tet_next.rotate(&r);
+        }
+        if tet_next.collides(&self.board) {
+            return false;
+        }
+
+        if let Some(m) = mov {
+            tet_next.mov(&m);
+        }
+        if tet_next.collides(&self.board) {
+            return false;
+        }
+
+        self.tetromino = tet_next;
+        true
+    }
+
+    pub fn get_tetromino(&self) -> &Tetromino {
+        &self.tetromino
+    }
+
+    pub fn get_tetromino_preview(&self) -> Tetromino {
+        let mut tet = self.tetromino.clone();
+        while !tet.collides(&self.board) {
+            tet.fall();
+        }
+        tet.mov(&Direction::Down);
+        tet
+    }
+
+    pub fn rush(&mut self) {
+        let mut prev = self.tetromino.clone();
+        self.tetromino.fall();
+        while !self.tetromino.collides(&self.board) {
+            self.tetromino.fall();
+            prev.fall();
+        }
+        if self.board.fill_tetromino_check_gameover(&prev) {
+            self.state = GameState::GameOver;
+        }
+        self.board.check_and_remove_lines();
+        self.tetromino = Tetromino::random(self.board.width / 2, 0);
+    }
+
+    pub fn step(&mut self) {
+        if self.state != GameState::Running {
+            return;
+        }
+        let prev = self.tetromino.clone();
+        self.tetromino.fall();
+        if self.tetromino.collides(&self.board) {
+            if self.board.fill_tetromino_check_gameover(&prev) {
+                self.state = GameState::GameOver;
+            }
+            self.board.check_and_remove_lines();
+            self.tetromino = Tetromino::random(self.board.width / 2, 0);
+        }
+    }
+
+    pub fn tile_color(&self, x: i32, y: i32) -> Color {
+        let b = &self.board;
+        b.tiles[b.p2i(&Point::from(x, y))].color.clone()
+    }
+
+    pub fn print(&self) {
+        for j in 0..self.board.height {
+            print!("{j:2}");
+            for i in 0..self.board.width {
+                let p = Point { x: i, y: j };
+                let mut c = ' ';
+                let t = &self.board.get_tile(p);
+                if t.filled {
+                    c = 'x';
+                } else {
+                    let mut idx = '0';
+                    for tp in self.tetromino.points() {
+                        idx = (idx as u8 + 1) as char;
+                        if tp == p {
+                            c = idx;
+                            break;
+                        }
+                    }
+                }
+                print!("{c}");
+            }
+            println!();
+        }
+        println!(
+            "  {}",
+            String::from_iter((0..self.board.width).map(|e| (e + 0x30) as u8 as char))
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,249 +1,176 @@
-use ratatui::{Terminal, style::Color};
-use std::cmp::Eq;
-use std::ops::Add;
+use crate::lmtetris::Tetris;
 
-const TETS: [[Point; 4]; 7] = [I, J, L, O, S, T, Z];
+pub mod lmtetris;
 
-const I: [Point; 4] = [
-    Point { x: -1, y: 0 },
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 0 },
-    Point { x: 2, y: 0 },
-];
-const J: [Point; 4] = [
-    Point { x: -1, y: 0 },
-    Point { x: -1, y: 1 },
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 0 },
-];
-const L: [Point; 4] = [
-    Point { x: -1, y: 0 },
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 0 },
-    Point { x: 1, y: 1 },
-];
-const O: [Point; 4] = [
-    Point { x: 0, y: 0 },
-    Point { x: 0, y: 1 },
-    Point { x: 1, y: 0 },
-    Point { x: 1, y: 1 },
-];
-const S: [Point; 4] = [
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 0 },
-    Point { x: -1, y: 1 },
-    Point { x: 0, y: 1 },
-];
-const T: [Point; 4] = [
-    Point { x: -1, y: 0 },
-    Point { x: 0, y: 0 },
-    Point { x: 1, y: 0 },
-    Point { x: 0, y: 1 },
-];
-const Z: [Point; 4] = [
-    Point { x: -1, y: 0 },
-    Point { x: 0, y: 0 },
-    Point { x: 0, y: 1 },
-    Point { x: 1, y: 1 },
-];
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode, KeyEventKind};
+use ratatui::layout::{Constraint, Layout, Position};
+use ratatui::style::{Color, Modifier, Style, Stylize};
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::canvas::{Canvas, Rectangle};
+use ratatui::widgets::{Block, Paragraph};
 
-#[derive(Clone, Debug)]
-struct Tile {
-    filled: bool,
-    color: Color,
-}
+use ratatui::{DefaultTerminal, Frame};
 
-#[derive(Default, PartialEq, Eq, Clone, Copy)]
-struct Point {
-    x: i32,
-    y: i32,
-}
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
-impl Add for Point {
-    type Output = Point;
-
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-
-impl Add<(i32, i32)> for Point {
-    type Output = Point;
-    fn add(self, other: (i32, i32)) -> Point {
-        Point {
-            x: self.x + other.0,
-            y: self.y + other.1,
-        }
-    }
-}
-
-#[derive(Default, Clone)]
-struct Tetromino {
-    offset: [Point; 4],
-    center: Point,
-    color: Color,
-}
-
-impl Tetromino {
-    fn random(x: i32, y: i32) -> Self {
-        Self {
-            offset: TETS[rand::random_range(0..TETS.len())],
-            center: Point { x, y },
-            color: Color::Indexed(rand::random::<u8>()),
-        }
-    }
-    fn fall(&mut self) -> &Self {
-        for p in &mut self.offset {
-            *p = *p + (0, 1);
-        }
-        self
-    }
-    fn collides(&self, board: &Board) -> bool {
-        for p in &self.offset {
-            if board.get_tile(*p + self.center).filled {
-                return true;
-            }
-            if p.y >= board.height - 1 {
-                return true;
-            }
-        }
-        false
-    }
-}
-
-#[derive(Debug)]
-struct Board {
-    tiles: Vec<Tile>,
-    width: i32,
-    height: i32,
-}
-
-#[derive(PartialEq)]
-enum GameState {
-    Running,
-    Paused,
-    GameOver,
-}
-
-struct Game {
-    state: GameState,
-    board: Board,
-    tetromino: Tetromino,
-}
-
-impl Board {
-    fn new(width: i32, height: i32) -> Self {
-        assert!(width >= 4);
-        assert!(height >= 4);
-        Board {
-            tiles: vec![
-                Tile {
-                    filled: false,
-                    color: Color::Black
-                };
-                (width * height) as usize
-            ],
-            width,
-            height,
-        }
-    }
-    fn p2i(&self, p: &Point) -> usize {
-        assert!(p.x >= 0);
-        assert!(p.y >= 0);
-        assert!(p.x < self.width);
-        assert!(p.y < self.height);
-        (p.x + p.y * self.width) as usize
-    }
-    fn get_tile(&self, p: Point) -> &Tile {
-        &self.tiles[self.p2i(&p)]
-    }
-    fn fill_tetromino_check_gameover(&mut self, t: &Tetromino) -> bool {
-        for p in t.offset {
-            let i = self.p2i(&(p + t.center));
-            let t = &mut self.tiles[i];
-            if t.filled {
-                return true;
-            } else {
-                t.filled = true;
-            }
-        }
-        false
-    }
-}
-
-impl Game {
-    fn new(width: i32, height: i32) -> Self {
-        Game {
-            state: GameState::Paused,
-            board: Board::new(width, height),
-            tetromino: Tetromino::random(width / 2, 0),
-        }
-    }
-    fn start(&mut self) {
-        self.state = GameState::Running
-    }
-    fn stop(&mut self) {
-        self.state = GameState::Paused
-    }
-    fn is_running(&self) -> bool {
-        self.state == GameState::Running
-    }
-    fn is_gameover(&self) -> bool {
-        self.state == GameState::GameOver
-    }
-    fn step(&mut self) {
-        if self.state != GameState::Running {
-            return;
-        }
-        let prev = self.tetromino.clone();
-        self.tetromino.fall();
-        if self.tetromino.collides(&self.board) {
-            if self.board.fill_tetromino_check_gameover(&prev) {
-                self.state = GameState::GameOver;
-            }
-            self.tetromino = Tetromino::random(self.board.width / 2, 0);
-        }
-    }
-    fn draw(&self) {
-        for j in 0..self.board.height {
-            for i in 0..self.board.width {
-                let p = Point { x: i, y: j };
-                let mut c = ' ';
-                let t = &self.board.get_tile(p);
-                if t.filled {
-                    c = 'x';
-                } else {
-                    for mut tp in self.tetromino.offset {
-                        tp = tp + self.tetromino.center;
-                        if tp == p {
-                            c = 'o';
-                            break;
-                        }
-                    }
-                }
-                print!("{c}");
-            }
-            println!();
-        }
-    }
-}
-
-fn main() {
-    let mut game = Game::new(10, 20);
+fn main2() {
+    let mut game = Tetris::new(10, 20);
     game.start();
     let mut i = 0;
     println!("Hello, ratatetris!");
-    while !game.is_gameover() {
+    while game.is_running() {
         game.step();
-        println!("{i}");
-        game.draw();
+        println!();
+        println!("i = {i}");
+        println!();
+        game.print();
+
+        if rand::random_bool(0.5) {
+            game.move_tet(Some(lmtetris::Direction::Left), None);
+        } else {
+            game.move_tet(Some(lmtetris::Direction::Right), None);
+        }
         i += 1;
-        if i > 200 {
+        if i > 500 {
             game.stop();
         }
     }
     println!("Game Over!");
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    ratatui::run(|terminal| App::new().run(terminal))
+}
+
+/// App holds the state of the application
+struct App {
+    game: Tetris,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            game: Tetris::new(10, 20),
+        }
+    }
+
+    fn move_left(&mut self) {
+        self.game.move_tet(Some(lmtetris::Direction::Left), None);
+    }
+
+    fn move_right(&mut self) {
+        self.game.move_tet(Some(lmtetris::Direction::Right), None);
+    }
+
+    fn rotate_left(&mut self) {
+        self.game.move_tet(None, Some(lmtetris::Direction::Left));
+    }
+
+    fn rotate_right(&mut self) {
+        self.game.move_tet(None, Some(lmtetris::Direction::Left));
+    }
+
+    fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        self.game.start();
+        let tick_rate = Duration::from_millis(1000 / 2);
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+            if !event::poll(timeout)? {
+                terminal.draw(|frame| self.render(frame))?;
+                self.game.step();
+                last_tick = Instant::now();
+                continue;
+            }
+            if let Some(key) = event::read()?.as_key_press_event() {
+                match key.code {
+                    KeyCode::Left => {
+                        self.move_left();
+                    }
+                    KeyCode::Right => {
+                        self.move_right();
+                    }
+                    KeyCode::Up => {
+                        self.rotate_right();
+                    }
+                    KeyCode::Char(' ') => {
+                        self.game.rush();
+                    }
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    KeyCode::Enter => {
+                        self.game = Tetris::new(10, 20);
+                        self.game.start();
+                    }
+                    _ => {}
+                }
+            }
+            terminal.draw(|frame| self.render(frame))?;
+        }
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        let layout = Layout::vertical([Constraint::Length(1), Constraint::Min(1)]);
+        let [header, game_area] = frame.area().layout(&layout);
+
+        let (msg, style) = (
+            vec![
+                "Press ".into(),
+                "q".bold(),
+                " to exit, ".into(),
+                "e".bold(),
+                " to start editing.".bold(),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        );
+        let text = Text::from(Line::from(msg)).patch_style(style);
+        let help_message = Paragraph::new(text);
+        frame.render_widget(help_message, header);
+        let game_view = Canvas::default()
+            .block(Block::bordered().title("LM Tetris"))
+            .x_bounds([0.0, (self.game.dimensions().0 as f64)])
+            .y_bounds([0.0, (self.game.dimensions().1 as f64)])
+            .paint(|c| {
+                for x in 0..self.game.dimensions().0 {
+                    for y in 0..self.game.dimensions().1 {
+                        c.draw(&Rectangle {
+                            x: x as f64 + 0.1,
+                            y: (self.game.dimensions().1 - y) as f64 + 0.1,
+                            width: 0.8,
+                            height: 0.8,
+                            color: Color::Indexed(self.game.tile_color(x, y).c),
+                        });
+                    }
+                }
+                let tet_preview = self.game.get_tetromino_preview();
+                for p in tet_preview.points() {
+                    let x = p.x;
+                    let y = p.y;
+                    c.draw(&Rectangle {
+                        x: x as f64 + 0.1,
+                        y: (self.game.dimensions().1 - y) as f64 + 0.1,
+                        width: 0.8,
+                        height: 0.8,
+                        color: Color::Indexed(tet_preview.color.c),
+                    })
+                }
+                let tet = self.game.get_tetromino();
+                for p in tet.points() {
+                    let x = p.x;
+                    let y = p.y;
+                    c.draw(&Rectangle {
+                        x: x as f64 + 0.1,
+                        y: (self.game.dimensions().1 - y) as f64 + 0.1,
+                        width: 0.8,
+                        height: 0.8,
+                        color: Color::Indexed(tet.color.c),
+                    })
+                }
+            });
+        frame.render_widget(game_view, game_area);
+    }
 }
