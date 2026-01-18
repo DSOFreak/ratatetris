52c34c4015dd8165004aef6aaca7bf7d724bfcd2
Lucien Müller <lcnmllr@gmail.com>
2026-01-19 00:03:35 +0100

    feat: addedd organ hello world

---
+++ src/lmtetris_sound.rs
@@ -0,0 +1,98 @@
+#![allow(clippy::precedence)]
+
+use std::thread;
+
+use assert_no_alloc::*;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{BufferSize, FromSample, SizedSample};
+use fundsp::prelude64::*;
+
+#[global_allocator]
+static A: AllocDisabler = AllocDisabler;
+
+const TETRIS_MELODY: [i32; 1] = [1];
+
+pub struct Sound {
+    fs: i32,
+}
+
+impl Sound {
+    pub fn new(fs: i32) -> Self {
+        Sound { fs }
+    }
+    pub fn start(&mut self) {
+        thread::spawn(|| {
+            let host = cpal::default_host();
+
+            let device = host
+                .default_output_device()
+                .expect("Failed to find a default output device");
+            let supported_config = device.default_output_config().unwrap();
+            let mut config = supported_config.config();
+            config.buffer_size = BufferSize::Fixed(256);
+            match supported_config.sample_format() {
+                cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()).unwrap(),
+                cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()).unwrap(),
+                cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()).unwrap(),
+                _ => panic!("Unsupported format"),
+            }
+        });
+    }
+    pub fn pause(&mut self) {}
+    pub fn swirl(&mut self) {}
+    pub fn combo(&mut self) {}
+    pub fn smash(&mut self) {}
+    pub fn tmove(&mut self) {}
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate as f64;
+    let channels = config.channels as usize;
+    let c = 0.2 * (organ_hz(midi_hz(57.0)) + organ_hz(midi_hz(61.0)) + organ_hz(midi_hz(64.0)));
+    let c = c >> pan(0.0);
+    let c = c >> (chorus(0, 0.0, 0.01, 0.2) | chorus(1, 0.0, 0.01, 0.2));
+    let mut c = c >> (declick() | declick()) >> (dcblock() | dcblock()) >> limiter_stereo(1.0, 5.0);
+
+    c.set_sample_rate(sample_rate);
+    c.allocate();
+
+    let mut next_value = move || assert_no_alloc(|| c.get_stereo());
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(50000));
+
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> (f32, f32))
+where
+    T: SizedSample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let sample = next_sample();
+        let left = T::from_sample(sample.0);
+        let right: T = T::from_sample(sample.1);
+
+        for (channel, sample) in frame.iter_mut().enumerate() {
+            if channel & 1 == 0 {
+                *sample = left;
+            } else {
+                *sample = right;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crate::lmtetris::Tetris;
+use crate::lmtetris_sound::Sound;
 
 pub mod lmtetris;
+pub mod lmtetris_sound;
 pub mod lmtetris_ui;
 
 use color_eyre::Result;
@@ -17,12 +19,14 @@ fn main() -> Result<()> {
 /// App holds the state of the application
 struct App {
     game: Tetris,
+    sound: Sound,
 }
 
 impl App {
     fn new() -> Self {
         Self {
             game: Tetris::new(10, 20),
+            sound: Sound::new(44100),
         }
     }
 
@@ -40,6 +44,7 @@ impl App {
 
     fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         self.game.start();
+        self.sound.start();
         let tick_rate = Duration::from_millis(1000 / 2);
         let mut last_tick = Instant::now();
         loop {
@@ -54,24 +59,31 @@ impl App {
                 match key.code {
                     KeyCode::Left => {
                         self.move_left();
+                        self.sound.tmove();
                     }
                     KeyCode::Right => {
                         self.move_right();
+                        self.sound.tmove();
                     }
                     KeyCode::Up => {
                         self.rotate_right();
+                        self.sound.swirl();
                     }
                     KeyCode::Down => {
                         self.game.step();
+                        self.sound.combo();
                     }
                     KeyCode::Char(' ') => {
                         self.game.rush();
+                        self.sound.smash();
                     }
                     KeyCode::Char('p') => {
                         if self.game.is_running() {
                             self.game.pause();
+                            self.sound.pause();
                         } else {
                             self.game.start();
+                            self.sound.start();
                         }
                     }
                     KeyCode::Char('q') => {
@@ -80,6 +92,7 @@ impl App {
                     KeyCode::Enter => {
                         self.game = Tetris::new(10, 20);
                         self.game.start();
+                        self.sound.start();
                     }
                     _ => {}
                 }
